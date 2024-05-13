c2a4e60500f6e59a7a5838fd265efec1731f8ea2
Ante Kordic <ante.kordic@yahoo.com>
2024-05-13 20:50:16 +0200

    api handling added

---
+++ backend/src/configs/tmdb.config.ts
@@ -0,0 +1,9 @@
+// import {connect, ConnectOptions} from "mongoose";
+
+// export const tmdbConnect = () => {
+//     connect(process.env.TMDB_API_KEY!, {
+//     } as ConnectOptions).then(
+//         () => console.log("tmdb connected successefully"),
+//         (error) => console.log(error)
+//     )
+// }
--- backend/src/routers/movie.router.ts
+++ backend/src/routers/movie.router.ts
@@ -1,44 +1,105 @@
-import {Router} from "express";
+import { Router } from "express";
 import { sample_movies } from "../data";
-import asynceHandler from 'express-async-handler';
-import { MovieModel } from "../models/movie.model";
-
+// import asynceHandler from 'express-async-handler';
+// import { isMethodDeclaration } from "typescript";
+import fetch from "node-fetch";
 
 const router = Router();
 
-router.get("/seed", asynceHandler(
-    async (req, res) => {
-       const movieCount = await MovieModel.countDocuments();
-       if(movieCount> 0){
-         res.send("Seed is already done!");
-         return;
-       }
-   
-       await MovieModel.create(sample_movies);
-       res.send("Seed Is Done!");
-   }
-   ))
+let currentPage = 1;
+let movieIndex = 0;
+let movies: string | any[] = [];
 
-   router.get("/",asynceHandler(
-    async (req, res) => {
-      const movies = await MovieModel.find();
-        res.send(movies);
-    }
-  ))
+router.get("/popular-movies/next", async (req, res) => {
+  // If there are no movies left in the current page, fetch the next page
+  if (movieIndex >= movies.length) {
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=${currentPage}&sort_by=popularity.desc`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.TMDB_API_KEY!,
+      },
+    };
 
-  router.get("/search/:searchTerm", asynceHandler(
-    async (req, res) => {
-      const searchRegex = new RegExp(req.params.searchTerm, 'i');
-      const movies = await MovieModel.find({title: {$regex:searchRegex}})
-      res.send(movies);
-    }
-  ))
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
 
-  router.get("/:movieId", asynceHandler(
-    async (req, res) => {
-      const movie = await MovieModel.findById(req.params.movieId);
-      res.send(movie);
+      if (data.results && data.results.length > 0) {
+        movies = data.results;
+        movieIndex = 0;
+        currentPage++;
+      } else {
+        return res.status(404).json({ error: "No movies found" });
+      }
+    } catch (error) {
+      console.error("Error fetching top-rated movies:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-  ))
+  }
+
+  // Send the next movie to the frontend
+  const nextMovie = movies[movieIndex];
+  res.json(nextMovie);
+  movieIndex++;
+});
+
+router.get("/:movieId", async (req, res) => {
+  try {
+    const movieId = req.params.movieId;
+    const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&append_to_response=videos`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.TMDB_API_KEY!,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default router;
+
+// router.get('/top-rated-movies/next', async (req, res) => {
+//   // If there are no movies left in the current page, fetch the next page
+//   if (movieIndex >= movies.length) {
+
+//     const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${currentPage}`;
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzkyNDdmZGI3NGQwZjA3ZWJkMDY5NjgxMDE4N2Y2OSIsInN1YiI6IjY1Y2E4YzY1YTIyZDNlMDE3YjRmYmJjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Go81zEmCmVngHKLDR-qAGW2SBzDbAYBE54Nwoqa2oWg'
+//       }
+//     };
+
+//     try {
+//       const response = await fetch(url, options);
+//       const data = await response.json();
+
+//       if (data.results && data.results.length > 0) {
+//         movies = data.results;
+//         movieIndex = 0;
+//         currentPage++;
+//       } else {
+//         return res.status(404).json({ error: 'No movies found' });
+//       }
+//     } catch (error) {
+//       console.error('Error fetching top-rated movies:', error);
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   }
+
+//   // Send the next movie to the frontend
+//   const nextMovie = movies[movieIndex];
+//   res.json(nextMovie);
+//   movieIndex++;
+// });
