92ee9578f85991b1d12e6b174972e04d6667891e
Ante Kordic <ante.kordic@yahoo.com>
2024-05-14 16:04:14 +0200

    watched movies, and watch later added

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,22 +1,29 @@
-import {Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
-export interface User{
-    id:string;
-    email:string;
-    password:string;
-    }
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+  watchedMovies: string[];
+  watchLaterMovies: string[];
+}
 
-export const UserSchema = new Schema<User>({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    }, {
-        timestamps: true,
-        toJSON:{
-            virtuals: true
-        },
-        toObject:{
-            virtuals: true
-    }
-});
-    
-export const UserModel = model<User>('user', UserSchema);
+export const UserSchema = new Schema<User>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    watchedMovies: [{ type: String }],
+    watchLaterMovies: [{ type: String }],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+export const UserModel = model<User>("user", UserSchema);
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,77 +1,147 @@
-import {Router} from 'express';
-import { sample_users } from '../data';
-import jwt from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
-import { User, UserModel } from '../models/user.model';
-import { HTTP_BAD_REQUEST } from '../constants/http_status';
-import bcrypt from 'bcryptjs';
+import { Router } from "express";
+import { sample_users } from "../data";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
-router.get("/seed", asyncHandler(
-  async (req, res) => {
-     const usersCount = await UserModel.countDocuments();
-     if(usersCount> 0){
-       res.send("Seed is already done!");
-       return;
-     }
- 
-     await UserModel.create(sample_users);
-     res.send("Seed Is Done!");
- }
- ))
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send("Seed is already done!");
+      return;
+    }
 
-router.post("/login", asyncHandler(
-  async (req, res) => {
-    const {email, password} = req.body;
-    const user = await UserModel.findOne({email});
-  
-     if(user && (await bcrypt.compare(password,user.password))) {
+    await UserModel.create(sample_users);
+    res.send("Seed Is Done!");
+  })
+);
+
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
       res.send(generateTokenReponse(user));
-     }
-     else{
-       res.status(HTTP_BAD_REQUEST).send("Username or password is invalid!");
-     }
-  
-  }
-))
-  
-router.post('/register', asyncHandler(
-  async (req, res) => {
-    const {email, password} = req.body;
-    const user = await UserModel.findOne({email});
-    if(user){
-      res.status(HTTP_BAD_REQUEST)
-      .send('User already exist, please login!');
+    } else {
+      res.status(HTTP_BAD_REQUEST).send("Username or password is invalid!");
+    }
+  })
+);
+
+router.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      res.status(HTTP_BAD_REQUEST).send("User already exist, please login!");
       return;
     }
 
     const encryptedPassword = await bcrypt.hash(password, 10);
 
-    const newUser:User = {
-      id:'',
+    const newUser: User = {
+      id: "",
       email: email.toLowerCase(),
       password: encryptedPassword,
-    }
+      watchedMovies: [],
+      watchLaterMovies: [],
+    };
 
     const dbUser = await UserModel.create(newUser);
     res.send(generateTokenReponse(dbUser));
-  }
-))
+  })
+);
 
-  const generateTokenReponse = (user : User) => {
-    const token = jwt.sign({
-      id: user.id, email:user.email
-    },process.env.JWT_TOKEN!,{
-      expiresIn:"30d"
-    });
-  
-    return {
+const generateTokenReponse = (user: User) => {
+  const token = jwt.sign(
+    {
       id: user.id,
       email: user.email,
-      token: token
-    };
-  }
-  
+    },
+    process.env.JWT_TOKEN!,
+    {
+      expiresIn: "30d",
+    }
+  );
 
-  export default router;
+  return {
+    id: user.id,
+    email: user.email,
+    token: token,
+  };
+};
+
+// Define a route for adding movie IDs to the user's watched list
+router.post("/watched", async (req, res) => {
+  try {
+    const { email, movieId } = req.body;
+
+    // Find the user by email
+    let user = await UserModel.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Check if the movie ID already exists in the watched list
+    if (user.watchedMovies.includes(movieId)) {
+      return res
+        .status(400)
+        .json({ error: "Movie ID already exists in the watched list" });
+    }
+
+    // Add the movie ID to the watched list and save the user
+    user.watchedMovies.push(movieId);
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Movie ID added to watched list successfully",
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Define a route for adding movie IDs to the user's watch later list
+router.post("/watchLater", async (req, res) => {
+  try {
+    const { email, movieId } = req.body;
+
+    // Find the user by email
+    let user = await UserModel.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Check if the movie ID already exists in the watch later list
+    if (user.watchLaterMovies.includes(movieId)) {
+      return res
+        .status(400)
+        .json({ error: "Movie ID already exists in the watch later list" });
+    }
+
+    // Add the movie ID to the watch later list and save the user
+    user.watchLaterMovies.push(movieId);
+    await user.save();
+
+    res.json({
+      success: true,
+      message: "Movie ID added to watch later list successfully",
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+export default router;
