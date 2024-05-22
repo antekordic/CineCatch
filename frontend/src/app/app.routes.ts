b66b2ae88080ab137677c22f0f5d56e3d98da629
Susanne <53381520+susiverse@users.noreply.github.com>
2024-05-22 12:06:59 +0200

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
-import {TinderComponent} from './pages/tinder/tinder.component';
-import {MoviesComponent} from "./pages/movies/movies.component";
-import {SearchComponent} from "./pages/search/search.component";
-import {MovieComponent} from "./pages/movie/movie.component";
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MoviesComponent } from "./pages/movies/movies.component";
+import { SearchComponent } from "./pages/search/search.component";
+import { MovieComponent } from "./pages/movie/movie.component";
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { authGuard } from './guards/auth.guard';
+import { AuthType } from "./enums/auth-type.enum";
 
 export const routes: Routes = [{
   path: '',
-  component: TinderComponent,
+  component: DashboardComponent,
   canActivate: [authGuard],
 }, {
   path: 'movies',
@@ -18,7 +19,9 @@ export const routes: Routes = [{
 }, {
   path: 'movies/:id',
   component: MovieComponent,
-  canActivate: [authGuard],
+  canActivate: [authGuard], data: {
+    authType: AuthType.Protected,
+  }
 }, {
   path: 'search',
   component: SearchComponent,
